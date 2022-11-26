3a2beaa58cb4fd838ec84834e18577215cebae71
bibinvc <bvcbibin95@gmail.com>
2022-11-26 19:01:12 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $(".about-info-inner").typed({
+    strings: [
+      "<span> Guruvayur</span> is a pilgrimage town in the southwest Indian state of Kerala. It’s known for centuries-old, red-roofed Guruvayur Temple, where Hindu devotees make offerings of fruit, spices or coins, often equivalent to their own weight. Nearby, Mammiyur Mahadeva Temple contains shrines to the deities Vishnu and Shiva. South of town, St. Thomas Church is believed to have been established by the apostle St. Thomas in 52 AD. ",
+    ],
+    typeSpeed: 7,
+    contentType: "html",
+  });
+}, 100);
