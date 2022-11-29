02db072f06425b15a0e988c0ec5f0ebe1725b381
bibinvc <bvcbibin95@gmail.com>
2022-11-29 12:33:27 +0530

    seond

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function () {
-  $(".about-info-inner").typed({
+  $(".mahatmyam-info-inner").typed({
     strings: [
-      "<span> Guruvayur</span> is a pilgrimage town in the southwest Indian state of Kerala. It’s known for centuries-old, red-roofed Guruvayur Temple, where Hindu devotees make offerings of fruit, spices or coins, often equivalent to their own weight. Nearby, Mammiyur Mahadeva Temple contains shrines to the deities Vishnu and Shiva. South of town, St. Thomas Church is believed to have been established by the apostle St. Thomas in 52 AD. ",
+      "<span> Mahatmyam </span> From the time immemorial, India has been a spiritually enlightened country consecrated by Rishis and temples of worship. Holy places like Kashi and Rameswaram have magnificent Temples which, besides their spiritual significance, are noted for their antiquity and historical importance. The artistic excellence and the architectural specialities of Konark and Khajuraho are so indescribable that they always attract not only the pilgrims from within the country but also swarms of tourists from abroad. But Guruvayur is a pilgrim center which has its own glory and greatness, mainly because of the divinity enshrined in the Sanctum - Sanctorum (central shrine) of the temple and the unique charm of the idol installed in it. The presiding deity in the Garbhagraha (central shrine) is Mahavishnu, worshipped according to the pooja routines laid down by Adi Sankaracharya and later written in to the Tantrasamuchaya by Chennas Narayanan Namboodiri (born in 1427). The Chennas Namboodiris are the hereditary Tantri of Guruvayur temple. The people at large, however, invoke the Lord as UNNIKRISHNA or BALAKRISHNA. ",
     ],
     typeSpeed: 7,
     contentType: "html",
---
+++ js/autoTypingAnimationS.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $(".history-info-inner").typed({
+    strings: [
+      "<span>Early history</span>According to the legends, the idol worshipped here is more than 5000 years old. But there are no historical records to establish it. In the 14th century Tamil literature 'Kokasandesam', references about a place called Kuruvayur is made. As early as 16th century (50 years after the Narayaneeyam was composed) many references are seen about Kuruvayur. In ancient Dravidic, Kuruvai means sea, hence the village on the coast may be called Kuruvayur. But according to Prof. K V Krishna Iyer (eminent historian), the Brahmins had begun to come and settle at Kodungalloor during the period of Chandra Gupta Maurya ( 321-297 BC). Trikkunavay in the Guruvayur documents is the same as Thrikkanamathilakam or Mathilakam mentioned in the Dutch and British records. And this place was in between Guruvayur and Kodungalloor. Guruvayur was Trikkunavay's subordinate shrine since they were destroyed by the Dutch in 1755. That way Guruvayur must have come into existence before 52 AD. The story of Pandyan King building a shrine here may be a reference to the Azhavars , but they are all silent in their writing about Guruvayur. It was Melpathur's Narayaneeyam through which the Temple got publicity. The concept of Unnikrishna popularised by Poonthanam , Kururamma , and Villwamangalam brought more and more devotees to Guruvayur.",
+    ],
+    typeSpeed: 7,
+    contentType: "html",
+  });
+}, 100);
